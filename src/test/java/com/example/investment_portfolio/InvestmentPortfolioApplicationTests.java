0521d6b0a9d470824674f318ba64cb6c976732a3
Ran Indra <ranindrar5@gmail.com>
2026-02-03 16:17:57 +0700

    Initial commit

---
+++ src/test/java/com/example/investment_portfolio/InvestmentPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.investment_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
